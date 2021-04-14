c495e770d6c813399eff52351ff2fb3591aef864
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-04-13 23:24:21 -0300

    feat(swap-forms): checkbox com controle do estado misto mantido em memória

--- libs/swap/forms/src/lib/directives/checkbox-group.directive.ts
+++ libs/swap/forms/src/lib/directives/checkbox-group.directive.ts
@@ -10,12 +10,34 @@ import {
   HostListener,
 } from '@angular/core';
 
-@Directive({
-  selector: 'input[swap-checkbox-group]',
-})
+type CheckboxGroupValue = Record<string, boolean>;
+type CheckboxGroupState = 'checked' | 'indeterminate';
+
+@Directive({ selector: 'input[swap-checkbox-group]' })
 export class CheckboxGroupDirective implements OnInit, OnDestroy {
+  /**
+   * Usado para manter o observable ativo
+   * enquanto temos ciclo de vida do componente
+   *
+   * @private
+   */
   private destroy = new Subject<void>();
-  private history: string[] = [];
+
+  /**
+   * Usado para manter o último estado misto
+   * em memória para retorno posterior
+   *
+   * @type {CheckboxGroupValue}
+   */
+  lastState: CheckboxGroupValue = null;
+
+  /**
+   * Cria uma instância para CheckboxGroupDirective
+   *
+   * @param {ElementRef<HTMLElement>} element
+   * @param {Renderer2} renderer
+   * @param {FormGroupName} container
+   */
   constructor(
     private element: ElementRef<HTMLElement>,
     private renderer: Renderer2,
@@ -23,74 +45,113 @@ export class CheckboxGroupDirective implements OnInit, OnDestroy {
   ) {}
 
   /**
-   * Controle do checkbox group baseado
-   * em alterações de controles filhos.
+   * Checkboxes controlados
+   *
+   * A partir de alterações individuais nos checkboxes
+   * alteramos o estado do controlador para quando:
+   *
+   * - Todos marcados: marcado
+   * - Todos desmarcados: desmarcado
+   * - Partialmente marcados: indeterminado / Misto
    */
   ngOnInit(): void {
-    if (this.element) {
-      console.log(this.element.nativeElement.parentElement.nextSibling);
-    }
-    var treeWalker = document.createTreeWalker(
-      this.element.nativeElement.parentElement.nextSibling,
-      NodeFilter.SHOW_ELEMENT,
-      {
-        acceptNode: function (node) {
-          return NodeFilter.FILTER_ACCEPT;
-        },
-      }
-    );
+    this.checkState(this.container.value);
 
-    var nodeList = [];
-    var currentNode = treeWalker.currentNode;
-
-    while (currentNode) {
-      nodeList.push(currentNode);
-      currentNode = treeWalker.nextSibling();
-    }
-
-    console.log(this.element.nativeElement.closest('input').nextElementSibling);
-
-    if (this.container?.control) {
-      this.container.valueChanges
-        .pipe(takeUntil(this.destroy))
-        .subscribe((value: Record<string, boolean>) => {
-          const checked = this.getState(value);
-          this.updateProperty('checked', checked || false);
-          this.updateProperty('indeterminate', checked === null);
-        });
-    }
+    this.container.valueChanges
+      .pipe(takeUntil(this.destroy))
+      .subscribe((value: CheckboxGroupValue) => this.checkState(value));
   }
 
   /**
-   * Controle dos filhos baseado em
-   * alterações do checkbox group.
+   * Checkbox controlador
+   *
+   * Escutando alterações do checkbox controlador
+   * é possível manter em memória o estado inicial dos
+   * checkboxes controlados para alternar entre os estados
+   * marcados / desmarcados, então retornar ao estado inicial.
+   *
+   * @param {HTMLInputElement} { checked }
    */
   @HostListener('change', ['$event.target'])
   onChange({ checked }: HTMLInputElement) {
     this.updateProperty('indeterminate', false);
-    if (this.container?.control) {
+
+    if (this.lastState && !checked) {
       this.toggleGroup(this.container.control, checked);
+      return;
     }
+
+    if (!this.lastState && checked) {
+      this.lastState = this.container.value;
+      this.toggleGroup(this.container.control, checked);
+      return;
+    }
+
+    this.container.control.patchValue(this.lastState);
+    this.lastState = null;
   }
 
-  toggleGroup(group: FormGroup, checked: boolean) {
+  /**
+   * Percorre checkboxes alterando seus estados
+   *
+   * @param {FormGroup} group
+   * @param {boolean} checked
+   */
+  public toggleGroup(group: FormGroup, checked: boolean) {
     Object.values(group.controls).map((c) => c.setValue(checked));
   }
 
-  getState(value: Record<string, boolean>) {
-    const values = Object.values(value);
-    const some = values.some((value) => value);
-    const all = values.every((value) => value);
-    return all ? all : some ? null : !!some;
+  /**
+   * Altera o estado do controlador baseado em
+   * valores contidos nos checkboxes controlados
+   *
+   * @private
+   * @param {CheckboxGroupValue} value
+   */
+  private checkState(value: CheckboxGroupValue) {
+    const checked = this.getState(value);
+    this.updateProperty('checked', checked || false);
+    this.updateProperty('indeterminate', checked === null);
   }
 
+  /**
+   * Analisa checkboxes para determinar se todos
+   * estão marcados, desmarcados ou misto, retornando:
+   *
+   * - Todos marcados: true
+   * - Todos desmarcados: false
+   * - Misto: null
+   *
+   * @private
+   * @param {CheckboxGroupValue} value
+   */
+  private getState(value: CheckboxGroupValue) {
+    const values = Object.values(value);
+    const some = values.some((value) => value);
+    const every = values.every((value) => value);
+    return every ? every : some ? null : some;
+  }
+
+  /**
+   * Altera o atributo do elemento no dom
+   *
+   * @private
+   * @internal
+   * @param {CheckboxGroupState} property
+   * @param {boolean} value
+   */
   private updateProperty(
-    property: 'checked' | 'indeterminate',
+    property: CheckboxGroupState,
     value: boolean
   ) {
     this.renderer.setProperty(this.element.nativeElement, property, value);
   }
 
+  /**
+   * Completa o subject e finaliza o observable
+   *
+   * @internal
+   */
   ngOnDestroy(): void {
     this.destroy.next();
     this.destroy.complete();
--- libs/swap/forms/src/lib/directives/control.directive.ts
+++ libs/swap/forms/src/lib/directives/control.directive.ts
@@ -2,7 +2,7 @@ import { Directive, HostBinding } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
-  selector: '[swap-control],[swapControl]',
+  selector: '[swap-control],[swap-control]',
 })
 export class ControlDirective {
   @HostBinding('attr.placeholder')
